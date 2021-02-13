516f9535cfc8d1414d54af715008737b505b6d7c
Countister <40427402+Countister@users.noreply.github.com>
2021-02-13 12:33:56 +0100

    Update Socket client

--- azerty/azerty.cpp
+++ azerty/azerty.cpp
@@ -20,29 +20,14 @@ typedef int SOCKET;
 typedef struct sockaddr_in SOCKADDR_IN;
 typedef struct sockaddr SOCKADDR;
 
-#pragma region DEFINITION GLOBALES
-    SOCKET socket;
-    int server;
-    int portClient = 23;
-    char buffer[1024];
-    socklen_t size;
-    //sockaddr_in server_addr;
-    //sockaddr_in client_addr;
-#pragma endregion
-
 #pragma region STRUCTURES
-    struct tcp_Socket
+    struct Socket_Types
     {
-        int domain = AF_INET;        //Socket TCP/IP
-        int type = SOCK_STREAM;     //Protocole TCP IP
-        int protocol = 0;           //Socket par défaut
+        int domain;        //Socket TCP/IP
+        int type;     //Protocole TCP IP
+        int protocol;           //Socket par défaut
     };
-    struct unix_Socket
-    {
-        int domain = AF_UNIX;        //Socket Unix
-        int type = SOCK_STREAM;     //Protocole TCP IP
-        int protocol = 0;           //Socket par défaut
-    };
+
     struct sockaddr_in
     {
         short      sin_family;
@@ -53,24 +38,49 @@ typedef struct sockaddr SOCKADDR;
 
 #pragma endregion
 
+#pragma region DEFINITIONS GLOBALES
+    SOCKET socket;
+    int server;
+    int portClient = 23;
+    char buffer[1024];
+    socklen_t size;
+    Socket_Types tcp_Socket;
+    Socket_Types unix_Socket;
+    
+    //Paramétrage socket TCP (vers client)
+    tcp_Socket.domain = AF_INET;    //Socket TCP/IP
+    tcp_Socket.type = SOCK_STREAM;
+    tcp_Socket.protocol = 0;
+    //Paramétrage socket Unix (vers DB)
+    unix_Socket.domain = AF_UNIX;   //SOCKET Unix
+    unix_Socket.type = SOCK_STREAM;
+    unix_Socket.protocol = 0;
+#pragma endregion
+
+
 SOCKET creationSocket(){
     SOCKET sock;
+
+    //Création socket
     sock = socket(tcp_Socket.domain, tcp_Socket.type, tcp_Socket.protocol);
-    if (sock < 0)
+    
+    //Si socket invalide
+    if (sock == INVALID_SOCKET)
     {
         cout << "\nError establishing socket..." << endl;
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     else
-        cout << "\n=> Socket server has been created..." << endl;
+        cout << "\n=> Socket server has been created..." << endl;       //DEBUG
     
-    //Déclaration du socket
-    SOCKADDR_IN server_addr;
+    SOCKADDR_IN server_addr;        //Déclaration du socket
+
     //Initialisation du socket
-    server_addr.sin_addr.s_addr = INADDR_ANY;           //Adresse de serveur
+    server_addr.sin_addr.s_addr = INADDR_ANY;                   //Adresse de serveur
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(portClient);                   //Port d'écoute du client
     
+    //Si connexion du socket engendre une erreur
     if ((bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr))) == SOCKET_ERROR)
     {
         cout << "=> Error binding connection, the socket has already been established..." << endl;
@@ -78,64 +88,13 @@ SOCKET creationSocket(){
     }
     else
         cout << "=> Looking for clients..." << endl;
-    //size = sizeof(server_addr);       //Pas utile? 
     return sock;
 }
 
 
 int main()
 {
-    //std::cout << "yolo" << std::endl;
-    //int client, server;
-    //int port = 23;
-    //char buffer[1024];
-
-    // Initialisation de WinSock
     
-
-    //Creation du socket (SOCK_STREAM (TCP) ou SOCK_DGRAM (UDP))
-    //client = socket(AF_INET, SOCK_STREAM, 0);
-    /*if (client < 0)
-    {
-        cout << "\nError establishing socket..." << endl;
-        exit(1);
-    }
-    cout << "\n=> Socket server has been created..." << endl;*/
-
-    //Paramétrage du socket
-    /*server_addr.sin_addr.s_addr = INADDR_ANY;           //Adresse de serveur
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);                   //Port*/
-    
-    /*if ((bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr))) == SOCKET_ERROR)
-    {
-        cout << "=> Error binding connection, the socket has already been established..." << endl;
-        return -1;
-    }
-
-    size = sizeof(server_addr);
-    cout << "=> Looking for clients..." << endl;
-    listen(sock, 1);*/
-
-    //Database => include pqxx/pqxx
-    /*try
-    {
-        connection C ("dbname = testdb user = postgres password = cohondob hostaddr = 127.0.0.1 port = 5432");
-        if (C.is_open())
-        {
-            std::cout << "Connexion a la base de donnees etablie: " << C.dbname() << std::endl;
-        }
-        else
-        {
-            std::cout << "Can't open database" << std::endl;
-            return 1;
-        }
-    }
-    catch (const std::exception& e)
-    {
-        cerr << e.what() << std::endl;
-        return 1;
-    }*/
     socket = creationSocket();
     while (listen(socket, 1) != SOCKET_ERROR) /* Boucle infinie. Exercice : améliorez ce code. */
     {
